5f16173f6e0552f976d18bed79502699d955b2f5
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-12-30 18:53:31 +0100

    feat: turn bookings into a slice and add a for loop that breaks on tikets sold out

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	const conferenceName = "Go Conference 2024"
@@ -11,27 +14,48 @@ func main() {
 	fmt.Printf("We currently have %v tickets remaining out of %v\n", remainingTickets, conferenceTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userFirstName string
-	var userLastName string
-	var userEmail string
-	var userTickets uint
+	var bookings []string
 
-	fmt.Print("Please enter your first name: ")
-	fmt.Scanln(&userFirstName)
-	fmt.Print("Please enter your last name: ")
-	fmt.Scanln(&userLastName)
-	fmt.Print("Please enter your email address: ")
-	fmt.Scanln(&userEmail)
+	for {
+		var userFirstName string
+		var userLastName string
+		var userEmail string
+		var userTickets uint
 
-	fmt.Print("How many tickets would you like to purchase? ")
-	fmt.Scanln(&userTickets)
+		fmt.Print("Please enter your first name: ")
+		fmt.Scanln(&userFirstName)
+		fmt.Print("Please enter your last name: ")
+		fmt.Scanln(&userLastName)
+		fmt.Print("Please enter your email address: ")
+		fmt.Scanln(&userEmail)
 
-	if userTickets > remainingTickets {
-		fmt.Printf("Sorry %v, we only have %v tickets remaining\n", userFirstName, remainingTickets)
-	} else {
-		remainingTickets -= userTickets
-		fmt.Printf("Thank you %v, you have purchased %v tickets\n", userFirstName, userTickets)
-		fmt.Printf("You will receive an email to %v confirming your purchase\n", userEmail)
-		fmt.Printf("There are now %v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Print("How many tickets would you like to purchase? ")
+		fmt.Scanln(&userTickets)
+
+		if userTickets > remainingTickets {
+			fmt.Printf("Sorry %v, we only have %v tickets remaining\n", userFirstName, remainingTickets)
+		} else {
+			remainingTickets -= userTickets
+			bookings = append(bookings, userFirstName+" "+userLastName+" - "+userEmail+" ("+strconv.Itoa(int(userTickets))+" tickets)")
+			fmt.Printf("Thank you %v, you have purchased %v tickets\n", userFirstName, userTickets)
+			fmt.Printf("You will receive an email to %v confirming your purchase\n", userEmail)
+			fmt.Printf("There are now %v tickets remaining for %v\n", remainingTickets, conferenceName)
+		}
+
+		fmt.Print("\n\n")
+		fmt.Println("Current bookings:")
+		fmt.Println("-----------------")
+		for _, booking := range bookings {
+			if booking != "" {
+				fmt.Println(booking)
+			}
+		}
+		fmt.Println("-----------------")
+		fmt.Print("\n\n")
+
+		if remainingTickets == 0 {
+			fmt.Println("Sorry, we have sold out of tickets")
+			break
+		}
 	}
 }
